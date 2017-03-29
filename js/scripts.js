260b37245553b2d7718d1f06734cc56db5561ff5
Aubrey Masten and Sowmya Dinavahi <aubrey@>
2017-03-29 09:21:07 -0700

    Separate user interface logic and business logic on jquery to understand the BDD

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//business logic
+var leapYear = function(year){
+  if ((year%4===0 && year%100!=0) || year%400 === 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+//user interface logic
+$(document).ready(function(){
+  $("#leap-year").submit(function(event){
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
